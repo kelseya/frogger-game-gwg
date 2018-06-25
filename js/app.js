e2a78a24a306e983e64c4f632ba3085a32896fe6
kelseya <33046682+kelseya@users.noreply.github.com>
2018-06-25 14:52:00 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -43,5 +43,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-//    player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[e.keyCode]);
 });
